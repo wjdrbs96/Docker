02bb9ce94da6852b7585a6b45d07d7013c15b90f
wjdrbs96 <wjdrbs966@naver.com>
2021-03-26 15:40:08 +0900

    Docker Study

---
+++ web/app.js
@@ -0,0 +1,15 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true
+})
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'Hello Docker' }) })
+
+
+fastify.listen(3000, '0.0.0.0', function (err, address) {
+  if (err) { 
+    fastify.log.error(err);
+    process.exit(1);
+}
+fastify.log.info(`server listening on ${address}`) })
